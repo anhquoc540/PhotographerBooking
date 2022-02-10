7607a6238ec0a1419de486d19a8ba304567b0929
anhquoc540 <anhquocnguyen290@gmail.com>
2022-02-10 17:15:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/photographerbooking/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.photographerbooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+
+public class MainActivity extends AppCompatActivity {
+
+    MeowBottomNavigation bottomNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.ic_baseline_home_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.ic_baseline_calendar_24));
+        //bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.ic_baseline_bookmark_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(4, R.drawable.ic_baseline_notifications_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(5, R.drawable.ic_baseline_settings_24));
+
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                Fragment fragment = null;
+                switch (item.getId()) {
+                    case 1:
+                        fragment = new HomeFragment();
+                        break;
+                    case 2:
+                        fragment = new BookingFragment();
+                        break;
+                    case 4:
+                        fragment = new NotificationFragment();
+                        break;
+                    case 5:
+                        fragment = new SettingsFragment();
+                        break;
+                }
+                loadFragment(fragment);
+            }
+        });
+
+        bottomNavigation.setCount(1, "10");
+        bottomNavigation.show(2, true);
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(getApplicationContext()
+                    ,"You Clicked " + item.getId()
+                    ,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bottomNavigation.setOnReselectListener(new MeowBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(getApplicationContext()
+                        ,"You Reselected " + item.getId()
+                        ,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout, fragment)
+                .commit();
+    }
+}
